d0417194791d4959586ba121b5cf93884d5586d3
Leon Wright <wrightleon@icloud.com>
2016-10-07 01:12:12 -0500

    More Changes

--- esirom/settings.py
+++ esirom/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +26,7 @@ SECRET_KEY = ')x%@66ny0s+8+j&8a_1w4r$%2v802rqd@a)3u-%(z-^+#f5!n7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://esiromads.herokuapp.com]
 
 
 # Application definition
@@ -77,10 +78,10 @@ WSGI_APPLICATION = 'esirom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'esirom',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'jackinthebox@1',
-        # 'HOST': '',
+        'NAME': 'esirom',
+        'USER': 'postgres',
+        'PASSWORD': 'jackinthebox@1',
+        'HOST': '',
     }
 }
 
@@ -118,7 +119,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join((BASE_DIR), 'staticfiles')
+STATIC_URL = '/static/')
